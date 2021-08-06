abe906befc4432736c1c41840b46473eabf4c63f
Luis Martinez <malbertoa_11@hotmail.com>
2021-08-06 15:25:09 -0400

    first commit

---
+++ bot.ts
@@ -0,0 +1,40 @@
+import * as fs from 'fs';
+import * as Discord from 'discord.js';
+
+const epigramFile = fs.readFileSync('./epigrams.json');
+const epigrams = JSON.parse(epigramFile.toString()) as string[];
+
+console.log('Bot starting up...');
+
+const bot = new Discord.Client({
+  intents: [
+    Discord.Intents.FLAGS.GUILDS,
+    Discord.Intents.FLAGS.GUILD_MESSAGES
+  ]
+});
+
+bot.on('ready', () => {
+  console.log(`Bot successfully connected as ${bot.user ? bot.user.tag : ''}`);
+});
+
+bot.on('messageCreate', (message) => {
+  if (message.content.startsWith('/epigram')) {
+    message.channel.send(epigrams[Math.floor(Math.random() * epigrams.length)]);
+  }
+});
+
+function sendOutRandomEpigram() {
+  const date = new Date();
+  if (date.getHours() !== 10) { // 9AM
+    return;
+  }
+
+  bot.channels.cache.forEach((channel) => {
+    if (channel.type === 'GUILD_TEXT') {
+      (channel as Discord.TextChannel).send(epigrams[Math.floor(Math.random() * epigrams.length)]);
+    }
+  });
+}
+
+bot.login('ODczMTkzODUyMTg0NDUzMTcy.YQ03JQ.OF1lvwbeILbhg7aEah1t-BpAgDE');
+setInterval(sendOutRandomEpigram, 3600000); // every hr 
